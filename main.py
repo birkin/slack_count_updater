abfb5e17808b7245d1a652d308c3936e3d126e38
Birkin James Diana <birkin.diana@gmail.com>
2024-07-09 08:14:51 -0400

    adds comments.

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ load_dotenv()
 
 HH_COUNT_JSON_PATH = os.environ['HH_COUNT_JSON_PATH']
 
+## grap api data
 items_url = 'https://repository.library.brown.edu/api/search/?q=rel_is_member_of_collection_ssim:%22bdr:wum3gm43%22&rows=0'
 orgs_url = 'https://repository.library.brown.edu/api/search/?q=-rel_is_part_of_ssim:*+rel_is_member_of_collection_ssim:%22bdr:wum3gm43%22&rows=0'
-
 items_jdict = requests.get(items_url).json()
 orgs_jdict = requests.get(orgs_url).json()
 
+## extract counts
 items_count = items_jdict['response']['numFound']
 orgs_count = orgs_jdict['response']['numFound']
 
+## prep json
 data = { 'items_count': items_count, 'orgs_count': orgs_count }
 json_data: str = json.dumps( data, indent=2, sort_keys=True )
 
-## write to json file
+## save and output
 with open(HH_COUNT_JSON_PATH, 'w') as f:
     f.write(json_data)
+print( json_data )
+
+## eof
